2fce5724c40e156e74a5d9c926a5a1eaef2a8f4b
Rajneesh Kumar Anand <60222670+Rajneesh-dba@users.noreply.github.com>
2022-12-17 13:33:56 +0530

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,85 @@
+/*
+
+// Pyramid Program soln
+
+public class MyClass {
+    public static void main(String args[]) {
+      int n=5;
+      for(int i=1;i<=n;i++){
+          for(int j=1;j<=n-i;j++){
+              System.out.print(" ");
+          }
+          for(int j=1;j<=i;j++){
+              System.out.print("* ");
+          }
+          System.out.println();
+      }
+    }
+}
+
+*/
+
+
+/*
+// Right Triangle Star Pattern
+public class MyClass{
+    public static void main(String args[]){
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+*/
+
+
+/*
+ // Left Triangle Star Pattern
+ public class MyClass{
+     public static void main(String args[]){
+         int n=5;
+         for(int i=1;i<=n;i++){
+             
+             for(int j=n-i;j>0;j--){
+                 System.out.print("  ");
+             }
+             
+             for(int j=1;j<=i;j++){
+                 System.out.print("* ");
+             }
+             System.out.println();
+         }
+     }
+ }
+ */
+ 
+ 
+ /*
+ //Diamond Shape Pattern Program in Java
+ public class MyClass {
+    public static void main(String args[]) {
+      int n=5;
+      for(int i=1;i<=n;i++){
+          for(int j=1;j<=n-i;j++){
+              System.out.print(" ");
+          }
+          for(int j=1;j<=i;j++){
+              System.out.print("* ");
+          }
+          System.out.println();
+      }
+      for(int i=n-1;i>=1;i--){
+          for(int j=1;j<=n-i;j++){
+              System.out.print(" ");
+          }
+          for(int j=1;j<=i;j++){
+              System.out.print("* ");
+          }
+          System.out.println();
+      }
+    }
+}
+*/
